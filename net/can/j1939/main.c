1c033d6bc85724b4ae359f5972708989cb4c0891
Oleksij Rempel <o.rempel@pengutronix.de>
2018-01-24 12:06:59 +0100

    j1939: remove proc support
    to make upstreaming more simple, remove procfs support.
    
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- net/can/j1939/main.c
+++ net/can/j1939/main.c
@@ -30,9 +30,6 @@ MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("EIA Electronics (Kurt Van Dijck & Pieter Beyens)");
 MODULE_ALIAS("can-proto-" __stringify(CAN_J1939));
 
-static const char j1939_procname[] = "can-j1939";
-struct proc_dir_entry *j1939_procdir;
-
 /* LOWLEVEL CAN interface */
 
 /* CAN_HDR: #bytes before can_frame data part */
@@ -351,95 +348,12 @@ static struct notifier_block j1939_netdev_notifier = {
 	.notifier_call = j1939_netdev_notify,
 };
 
-/* proc access to the addr+name database */
-static int j1939_proc_show_addr(struct seq_file *sqf, void *v)
-{
-	struct net_device *netdev;
-	struct j1939_priv *priv;
-	int j;
-
-	seq_puts(sqf, "iface\tsa\t#users\n");
-	rcu_read_lock();
-	for_each_netdev_rcu(&init_net, netdev) {
-		priv = j1939_priv_get(netdev);
-		if (!priv)
-			continue;
-		read_lock_bh(&priv->lock);
-		for (j = 0; j < 0xfe; ++j) {
-			if (!priv->ents[j].nusers)
-				continue;
-			seq_printf(sqf, "%s\t%02x\t%i\n",
-				   netdev->name, j, priv->ents[j].nusers);
-		}
-		read_unlock_bh(&priv->lock);
-		j1939_priv_put(priv);
-	}
-	rcu_read_unlock();
-	return 0;
-}
-
-static int j1939_proc_show_name(struct seq_file *sqf, void *v)
-{
-	struct net_device *netdev;
-	struct j1939_priv *priv;
-	struct j1939_ecu *ecu;
-
-	seq_puts(sqf, "iface\tname\tsa\t#users\n");
-	rcu_read_lock();
-	for_each_netdev_rcu(&init_net, netdev) {
-		priv = j1939_priv_get(netdev);
-		if (!priv)
-			continue;
-		read_lock_bh(&priv->lock);
-		list_for_each_entry(ecu, &priv->ecus, list)
-			seq_printf(sqf, "%s\t%016llx\t%02x%s\t%i\n",
-				   netdev->name, ecu->name, ecu->sa,
-				   (priv->ents[ecu->sa].ecu == ecu) ? "" : "?",
-				   ecu->nusers);
-		read_unlock_bh(&priv->lock);
-		j1939_priv_put(priv);
-	}
-	rcu_read_unlock();
-	return 0;
-}
-
-static int j1939_proc_open_addr(struct inode *inode, struct file *file)
-{
-	return single_open(file, j1939_proc_show_addr, NULL);
-}
-
-static int j1939_proc_open_name(struct inode *inode, struct file *file)
-{
-	return single_open(file, j1939_proc_show_name, NULL);
-}
-
-static const struct file_operations j1939_proc_ops_addr = {
-	.owner		= THIS_MODULE,
-	.open		= j1939_proc_open_addr,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
-};
-
-static const struct file_operations j1939_proc_ops_name = {
-	.owner		= THIS_MODULE,
-	.open		= j1939_proc_open_name,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
-};
-
 /* MODULE interface */
 static __init int j1939_module_init(void)
 {
 	int ret;
 
 	pr_info("can: SAE J1939\n");
-
-	/* create /proc/net/can directory */
-	j1939_procdir = proc_mkdir(j1939_procname, init_net.proc_net);
-	if (!j1939_procdir)
-		return -EINVAL;
 
 	ret = register_netdevice_notifier(&j1939_netdev_notifier);
 	if (ret)
@@ -454,38 +368,23 @@ static __init int j1939_module_init(void)
 	if (ret < 0)
 		goto fail_tp;
 
-	if (!proc_create("addr", 0444, j1939_procdir, &j1939_proc_ops_addr))
-		goto fail_addr;
-	if (!proc_create("name", 0444, j1939_procdir, &j1939_proc_ops_name))
-		goto fail_name;
 	return 0;
 
- fail_name:
-	remove_proc_entry("addr", j1939_procdir);
- fail_addr:
-	j1939tp_module_exit();
  fail_tp:
 	can_proto_unregister(&j1939_can_proto);
  fail_sk:
 	unregister_netdevice_notifier(&j1939_netdev_notifier);
  fail_notifier:
-	proc_remove(j1939_procdir);
-	j1939_procdir = NULL;
 	return ret;
 }
 
 static __exit void j1939_module_exit(void)
 {
-	remove_proc_entry("name", j1939_procdir);
-	remove_proc_entry("addr", j1939_procdir);
 	j1939tp_module_exit();
 
 	can_proto_unregister(&j1939_can_proto);
 
 	unregister_netdevice_notifier(&j1939_netdev_notifier);
-
-	proc_remove(j1939_procdir);
-	j1939_procdir = NULL;
 }
 
 module_init(j1939_module_init);

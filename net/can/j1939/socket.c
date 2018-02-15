17888ab2b23e9efcd93a9f36a1c1f0eac4072326
Marc Kleine-Budde <mkl@pengutronix.de>
2018-02-15 09:37:11 +0100

    j1939: socket: Adopt j1939sk_getname() to new API
    Adopt j1939sk_getname() to new API introducted in:
    
        9b2c45d479d0 net: make getname() functions return length rather than use int* parameter
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- net/can/j1939/socket.c
+++ net/can/j1939/socket.c
@@ -342,8 +342,7 @@ static void j1939sk_sock2sockaddr_can(struct sockaddr_can *addr,
 	}
 }
 
-static int j1939sk_getname(struct socket *sock, struct sockaddr *uaddr,
-			   int *len, int peer)
+static int j1939sk_getname(struct socket *sock, struct sockaddr *uaddr, int peer)
 {
 	struct sockaddr_can *addr = (struct sockaddr_can *)uaddr;
 	struct sock *sk = sock->sk;
@@ -358,7 +357,7 @@ static int j1939sk_getname(struct socket *sock, struct sockaddr *uaddr,
 	}
 
 	j1939sk_sock2sockaddr_can(addr, jsk, peer);
-	*len = J1939_MIN_NAMELEN;
+	ret = J1939_MIN_NAMELEN;
 
  failure:
 	release_sock(sk);

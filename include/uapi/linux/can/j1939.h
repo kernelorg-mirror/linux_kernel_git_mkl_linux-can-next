33ee2ddd128678f03e7e1b558ef80b615ff44c18
Marc Kleine-Budde <mkl@pengutronix.de>
2018-03-07 10:28:13 +0100

    j1939: uapi: fix indention
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- include/uapi/linux/can/j1939.h
+++ include/uapi/linux/can/j1939.h
@@ -16,7 +16,7 @@
 #include <linux/socket.h>
 #include <linux/can.h>
 
-#define J1939_IDLE_ADDR	0xfe
+#define J1939_IDLE_ADDR 0xfe
 #define J1939_NO_ADDR 0xff
 #define J1939_NO_NAME 0
 #define J1939_NO_PGN 0x40000

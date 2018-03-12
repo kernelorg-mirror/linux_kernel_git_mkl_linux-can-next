cfce534a8cb14eb72ecb883a7f145d1816dc1a7c
Oleksij Rempel <o.rempel@pengutronix.de>
2018-03-12 14:23:41 +0100

    j1939: address-claim: check for addr.sa == J1939_IDLE_ADDR
    sa > J1939_IDLE_ADDR was already tested by j1939_address_is_valid()
    
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- net/can/j1939/address-claim.c
+++ net/can/j1939/address-claim.c
@@ -152,7 +152,7 @@ static void j1939_ac_process(struct j1939_priv *priv, struct sk_buff *skb)
 	if (IS_ERR(ecu))
 		goto done;
 
-	if (skcb->addr.sa >= J1939_IDLE_ADDR) {
+	if (skcb->addr.sa == J1939_IDLE_ADDR) {
 		j1939_ecu_unregister_locked(ecu);
 		goto done;
 	}

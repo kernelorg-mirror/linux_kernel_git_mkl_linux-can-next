e6a5fd45db012e6a20fd3fa11db15cd7255f882b
Marc Kleine-Budde <mkl@pengutronix.de>
2018-03-12 12:29:01 +0100

    j1939: priv: fix indention
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- net/can/j1939/j1939-priv.h
+++ net/can/j1939/j1939-priv.h
@@ -160,7 +160,7 @@ void j1939_ac_recv(struct j1939_priv *priv, struct sk_buff *skb);
  * when a matching ecu already exists, then that is returned
  */
 struct j1939_ecu *j1939_ecu_get_register_locked(struct j1939_priv *priv,
-					  name_t name, bool create_if_necessary);
+						name_t name, bool create_if_necessary);
 
 /* unregister must be called with lock held */
 void j1939_ecu_unregister_locked(struct j1939_ecu *ecu);

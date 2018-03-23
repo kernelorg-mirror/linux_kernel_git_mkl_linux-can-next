e82b28d0eec51a7d371b38fd12bf659724d9bfcf
Oleksij Rempel <o.rempel@pengutronix.de>
2018-03-23 09:11:16 +0100

    j1939: address-claim: j1939_ac_process(): cancel timer asap
    This is needed for proper ref counting.
    
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- net/can/j1939/address-claim.c
+++ net/can/j1939/address-claim.c
@@ -154,6 +154,9 @@ static void j1939_ac_process(struct j1939_priv *priv, struct sk_buff *skb)
 	if (IS_ERR_OR_NULL(ecu))
 		goto out_unlock_bh;
 
+	/* cancel pending (previous) address claim */
+	hrtimer_cancel(&ecu->ac_timer);
+
 	if (j1939_address_is_idle(skcb->addr.sa)) {
 		j1939_ecu_unregister_locked(ecu);
 		goto out_unlock_bh;
@@ -162,8 +165,6 @@ static void j1939_ac_process(struct j1939_priv *priv, struct sk_buff *skb)
 	/* save new addr */
 	if (ecu->addr != skcb->addr.sa)
 		j1939_ecu_unmap_locked(ecu);
-	/* cancel pending (previous) address claim */
-	hrtimer_cancel(&ecu->ac_timer);
 	ecu->addr = skcb->addr.sa;
 
 	prev = j1939_ecu_find_by_addr_locked(priv, skcb->addr.sa);

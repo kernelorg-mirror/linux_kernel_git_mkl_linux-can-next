80ba821486c3ab51300b7d162e0f7e5311d529ea
Oleksij Rempel <o.rempel@pengutronix.de>
2019-08-08 07:02:36 +0200

    j1939: j1939_sk_errqueue: add some debug
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- net/can/j1939/socket.c
+++ net/can/j1939/socket.c
@@ -875,6 +875,7 @@ void j1939_sk_errqueue(struct j1939_session *session,
 	struct j1939_sock *jsk;
 	struct sock_exterr_skb *serr;
 	struct sk_buff *skb;
+	char *state = "UNK";
 	int err;
 
 	/* currently we have no sk for the RX session */
@@ -904,6 +905,7 @@ void j1939_sk_errqueue(struct j1939_session *session,
 		serr->ee.ee_errno = ENOMSG;
 		serr->ee.ee_origin = SO_EE_ORIGIN_TIMESTAMPING;
 		serr->ee.ee_info = SCM_TSTAMP_ACK;
+		state = "ACK";
 		break;
 	case J1939_ERRQUEUE_SCHED:
 		if (!(sk->sk_tsflags & SOF_TIMESTAMPING_TX_SCHED))
@@ -912,11 +914,13 @@ void j1939_sk_errqueue(struct j1939_session *session,
 		serr->ee.ee_errno = ENOMSG;
 		serr->ee.ee_origin = SO_EE_ORIGIN_TIMESTAMPING;
 		serr->ee.ee_info = SCM_TSTAMP_SCHED;
+		state = "SCH";
 		break;
 	case J1939_ERRQUEUE_ABORT:
 		serr->ee.ee_errno = session->err;
 		serr->ee.ee_origin = SO_EE_ORIGIN_LOCAL;
 		serr->ee.ee_info = J1939_EE_INFO_TX_ABORT;
+		state = "ABT";
 		break;
 	default:
 		netdev_err(priv->ndev, "Unknown errqueue type %i\n", type);
@@ -926,6 +930,7 @@ void j1939_sk_errqueue(struct j1939_session *session,
 	if (sk->sk_tsflags & SOF_TIMESTAMPING_OPT_ID)
 		serr->ee.ee_data = session->tskey;
 
+	netdev_dbg(session->priv->ndev, "%s: 0x%p tskey: %i, state: %s\n", __func__, session, session->tskey, state);
 	err = sock_queue_err_skb(sk, skb);
 
 	if (err)
